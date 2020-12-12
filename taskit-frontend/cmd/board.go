80581e9b901b13284720cef694deef9e5cf8180c
Luiz <luiz.felipe.556@outlook.com>
2020-12-12 14:10:17 -0300

    list all tasks! using board command

--- taskit-frontend/cmd/board.go
+++ taskit-frontend/cmd/board.go
@@ -1,10 +1,19 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
+
+// Task Struct
+type Task struct {
+	Rowid       int    `json:"rowid"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
 
 // boardCmd represents the board command
 var boardCmd = &cobra.Command{
@@ -12,7 +21,7 @@ var boardCmd = &cobra.Command{
 	Short: "List all the tasks",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		listAllTasks()
+		getAllTasks()
 	},
 }
 
@@ -20,6 +29,26 @@ func init() {
 	rootCmd.AddCommand(boardCmd)
 }
 
-func listAllTasks() {
-	fmt.Println("board called")
+// Get all tasks from localhost webserver
+func getAllTasks() {
+	resp, err := http.Get("http://localhost:8080/tasks")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	var tasksArray []Task
+	e := json.NewDecoder(resp.Body).Decode(&tasksArray)
+	if e != nil {
+		fmt.Println(e)
+	}
+
+	for _, v := range tasksArray {
+		fmt.Printf("--------------------------\n")
+		fmt.Printf("ID: %v\n", v.Rowid)
+		fmt.Printf("Title: %v\n", v.Title)
+		fmt.Printf("Description: %v\n", v.Description)
+		fmt.Printf("--------------------------\n")
+	}
+
 }
